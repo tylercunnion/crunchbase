b55ebfab0a6085b505017000dd7eb7701cabdf89
Tyler Cunnion <tyler.cunnion@gmail.com>
2011-11-06 21:47:52 -0500

    Service providers initialize

--- lib/crunchbase/service_provider.rb
+++ lib/crunchbase/service_provider.rb
@@ -12,6 +12,23 @@ module Crunchbase
       return s
     end
     
+    def initialize(json)
+      @name = json["name"]
+      @permalink = json["permalink"]
+      @crunchbase_url = json["crunchbase_url"]
+      @homepage_url = json["homepage_url"]
+      @phone_number = json["phone_number"]
+      @tag_list = json["tag_list"]
+      @alias_list = json["alias_list"]
+      @created_at = DateTime.parse(json["created_at"])
+      @updated_at = DateTime.parse(json["updated_at"])
+      @overview = json["overview"]
+      @image = json["image"]
+      @offices = json["offices"]
+      @providerships = json["providerships"]
+      @external_links = json["external_links"]
+    end
+    
     
   end
 end
--- spec/crunchbase/service_provider_spec.rb
+++ spec/crunchbase/service_provider_spec.rb
@@ -8,12 +8,5 @@ module Crunchbase
       sp.name.should == "Fox Rothschild"
     end    
     
-    it "should return date for founded" do
-      sp = ServiceProvider.new({"founded_year" => 2004, "founded_month" => 2,
-        "founded_day" => 1, "created_at" => "Sat Dec 22 08:42:28 UTC 2007",
-        "updated_at" => "Sat Dec 22 08:42:28 UTC 2007"})
-      sp.founded.should === Date.new(2004, 2, 1)
-    end
-    
   end
 end
