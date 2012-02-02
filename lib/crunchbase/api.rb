df9a04a46b23c2f0ccc0225313d87ac087bdcbdc
Tyler Cunnion <tyler.cunnion@gmail.com>
2012-02-02 16:59:19 -0500

    Chose marginally better exception

--- lib/crunchbase/api.rb
+++ lib/crunchbase/api.rb
@@ -60,8 +60,7 @@ module Crunchbase
     end
 
     def self.fetch_but_follow_redirects(uri_str, limit = 10)
-      # You should choose better exception.
-      raise ArgumentError, 'HTTP redirect too deep' if limit == 0
+      raise CrunchException, 'HTTP redirect too deep' if limit == 0
 
       url = URI.parse(uri_str)
       req = Net::HTTP::Get.new(url.path, { 'User-Agent' => 'Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_3_2 like Mac OS X; en-us) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8H7 Safari/6533.18.5' })
