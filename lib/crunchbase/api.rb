51eff65bdff5802bc8ccab5e8ce6439f163b8856
Tyler Cunnion <tyler.cunnion@gmail.com>
2012-02-02 17:00:14 -0500

    No reason for using the iPhone User Agent string

--- lib/crunchbase/api.rb
+++ lib/crunchbase/api.rb
@@ -63,7 +63,7 @@ module Crunchbase
       raise CrunchException, 'HTTP redirect too deep' if limit == 0
 
       url = URI.parse(uri_str)
-      req = Net::HTTP::Get.new(url.path, { 'User-Agent' => 'Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_3_2 like Mac OS X; en-us) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8H7 Safari/6533.18.5' })
+      req = Net::HTTP::Get.new(url.path)
       response = Net::HTTP.start(url.host, url.port) { |http| http.request(req) }
       case response
       when Net::HTTPSuccess     then response
